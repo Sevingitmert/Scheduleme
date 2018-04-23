fdd9df3450495247c20dabb7ddd4dc37d8dca13d
Sevingitmert <37509518+Sevingitmert@users.noreply.github.com>
2018-04-23 14:31:11 +0300

    23.04 son değişiklikler

--- app/src/main/java/com/example/merts/scheduleme/ProfileActivity.java
+++ app/src/main/java/com/example/merts/scheduleme/ProfileActivity.java
@@ -1,15 +1,22 @@
 package com.example.merts.scheduleme;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import android.support.v7.widget.Toolbar;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     FirebaseAuth mAuth;
     FirebaseUser mUser;
@@ -20,17 +27,37 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mAuth=FirebaseAuth.getInstance();
-        mUser=mAuth.getCurrentUser();
-        profiletview=(TextView) findViewById(R.id.textView);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_draweer_open, R.string.navigation_draweer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        profiletview = (TextView) findViewById(R.id.textView);
 
         profiletview.setText(mUser.getEmail());
     }
 
-    public void signOut(View v){
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {  //açıp kapamayı left side da yapıyor. Sağ taraf için start yerine end
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void signOut(View v) {
         mAuth.signOut();
         finish();
-        Intent i = new Intent(this,MainActivity.class);
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
 }
