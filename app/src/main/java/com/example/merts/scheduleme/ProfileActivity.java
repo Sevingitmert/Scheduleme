4aa5a3cd86adbe3e54cdfc0914fa8a0b49dc4906
Sevingitmert <37509518+Sevingitmert@users.noreply.github.com>
2018-04-24 15:06:48 +0300

    Sayfa geçişleri tamam.

--- app/src/main/java/com/example/merts/scheduleme/ProfileActivity.java
+++ app/src/main/java/com/example/merts/scheduleme/ProfileActivity.java
@@ -1,11 +1,14 @@
 package com.example.merts.scheduleme;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,7 +17,7 @@ import android.support.v7.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -31,6 +34,8 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_draweer_open, R.string.navigation_draweer_close);
@@ -38,11 +43,50 @@ public class ProfileActivity extends AppCompatActivity {
         toggle.syncState();
 
 
-        mAuth = FirebaseAuth.getInstance();
-        mUser = mAuth.getCurrentUser();
-        profiletview = (TextView) findViewById(R.id.textView);
+        if (savedInstanceState == null) {
 
-        profiletview.setText(mUser.getEmail());
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new AddalarmFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_addalarm);
+        }
+
+
+        // mAuth = FirebaseAuth.getInstance();
+        // mUser = mAuth.getCurrentUser();
+        // profiletview = (TextView) findViewById(R.id.textView);
+        //profiletview.setText(mUser.getEmail());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_addalarm:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AddalarmFragment()).commit();
+                break;
+            case R.id.nav_addlocation:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AddlocationFragment()).commit();
+                break;
+            case R.id.nav_takenote:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TakenoteFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfilFragment()).commit();
+                break;
+
+            case R.id.nav_localactivity:
+                break;
+            case R.id.nav_signout:
+
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
