c1bbb5b3b328c9bad8f82c55d7f5e27db4b8f827
konyarfurkan <37509554+konyarfurkan@users.noreply.github.com>
2018-04-27 15:59:14 +0300

    note eklendi

---
+++ app/src/main/java/com/example/merts/scheduleme/Note.java
@@ -0,0 +1,55 @@
+package com.example.merts.scheduleme;
+
+import com.google.firebase.firestore.Exclude;
+
+/**
+ * Created by furkan on 26.04.2018.
+ */
+
+public class Note {
+    private String email;
+    private String documentId;
+    private String title;
+    private String description;
+
+
+    public Note(){
+        //public no-arg constructor needed
+    }
+
+    public Note(String email,String title, String description){
+        this.email = email;
+        this.title=title;
+        this.description=description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+
+}
