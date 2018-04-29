c5bdbee0c90366900b16bd59d801dade193718ee
Sevingitmert <37509518+Sevingitmert@users.noreply.github.com>
2018-04-29 22:55:46 +0300

    Webview Padding tamam

--- app/src/main/java/com/example/merts/scheduleme/LocalActivity.java
+++ app/src/main/java/com/example/merts/scheduleme/LocalActivity.java
@@ -1,9 +1,12 @@
 package com.example.merts.scheduleme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class LocalActivity extends AppCompatActivity {
     private WebView webView;
@@ -18,6 +21,15 @@ public class LocalActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://www.biletix.com/search/TURKIYE/tr?category_sb=-1&date_sb=-1&city_sb=Eski%C5%9Fehir#!city_sb:Eski%C5%9Fehir");
+
+        Button button = findViewById(R.id.button_goback);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LocalActivity.this, ProfileActivity.class );
+                startActivity(intent);
+            }
+        });
 
 
     }
